2cd8b456c8ef5dff3712b4813654ea7d19775cac
L.W.L <102495553@qq.com>
2018-07-31 11:01:58 +0800

    pm2 deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,31 @@
 module.exports = {
   apps: [
     {
-      name: "worker",
-      script: "./server.js",
+      name: 'App',
+      script: 'server.js',
       watch: true,
       env: {
-        "NODE_ENV": "development"
+        "COMMON_VARIABLE": true,
       },
-      env_production: {
+      "env_production": {
         "NODE_ENV": "production"
       }
     }
-  ]
+  ],
+  deploy: {
+    production: {
+      user: 'ecs_manager',
+      host: '120.26.235.4',
+      port: '22',
+      ref: 'origin/master',
+      repo: 'git@github.com:Lwenli1224/pm2-ecosystem.git',
+      path: '/www/app/production',
+      ssh_options: 'StrictHostKeyChecking=no',
+      "post-deploy": 'npm install; pm2 startOrRestart ecosystem.config.js --env production',
+      "pre-deploy-local": "echo 'Deploy Done!'",
+      "env": {
+        "NODE_ENV": "production"
+      }
+    }
+  }
 }
